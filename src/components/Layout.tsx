0f3bb70f657a652e97412e7134d5fec3dfa3dc21
akdl911215 <akdl911215@naver.com>
2025-04-28 14:59:40 +0900

    layout modify

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,8 +6,12 @@ type Props = { children: ReactNode }
 
 export default function Layout({ children }: Props) {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-amber-50 to-amber-100 flex flex-col">
-      <Nav />
+    <div className="h-screen overflow-hidden bg-gradient-to-b from-amber-50 to-amber-100 flex flex-col">
+            {/* sm 이하 (640px 미만) 에서는 숨기고, 그 이상이면 보여줌 */}
+      <div className="hidden sm:block">
+        <Nav />
+      </div>
+
       <main className="flex-grow flex justify-center items-center p-6">
         <div className="w-full max-w-md">
           {children}
